90a4a0a89fe3cf3fb9b748affdaca853a876dfd4
Joseph Alejandro Cardenas Palacio <joseph.cardenas2005@gmail.com>
2025-06-11 18:51:52 -0500

    actualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/pages/public/home/home';
+import { Login } from './component/pages/public/login/login';
+import { Registrer } from './component/pages/public/registrer/registrer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: Home },
+    {path: "login", component: Login },
+    {path: "registrer", component: Registrer },
+    {path: "**", redirectTo: "home", pathMatch: "full" },
+    {path: "", redirectTo: "home", pathMatch: "full" }
+];
