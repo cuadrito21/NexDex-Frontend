19aa9dff08cf8d189fac685514d9958e5047272a
Joseph Alejandro Cardenas Palacio <joseph.cardenas2005@gmail.com>
2025-06-11 18:36:13 -0500

    this is the fronted of NexDev

---
+++ src/app/component/pages/public/registrer/registrer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrer',
+  imports: [],
+  templateUrl: './registrer.html',
+  styleUrl: './registrer.css'
+})
+export class Registrer {
+
+}
