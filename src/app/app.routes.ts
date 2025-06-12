04f5a9dcf9b7f3702880f0b0ed2b44b714a8c40d
Cristhiangar <alejandrocrsthian823@gmail.com>
2025-06-11 21:47:04 -0500

    Creando la ruta de companyProfile y creando el html con las rutas predeterminadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home } from './component/pages/public/home/home';
 import { Login } from './component/pages/public/login/login';
 import { Registrer } from './component/pages/public/registrer/registrer';
 import { Offers } from './pages/private/offers/offers';
+import { CompanyProfile } from './pages/private/company-profile/company-profile';
 
 export const routes: Routes = [
     {path: "home", component: Home },
     {path: "login", component: Login },
     {path: "registrer", component: Registrer },
+    {path: "dashboard/companyProfile", component: CompanyProfile},
     {path: "dashboard/offers", component: Offers },
     {path: "**", redirectTo: "home", pathMatch: "full" },
     {path: "", redirectTo: "home", pathMatch: "full" }
---
+++ src/app/pages/private/company-profile/company-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-profile',
+  imports: [],
+  templateUrl: './company-profile.html',
+  styleUrl: './company-profile.css'
+})
+export class CompanyProfile {
+
+}
