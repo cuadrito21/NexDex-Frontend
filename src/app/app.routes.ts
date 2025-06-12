42708c5b7b9b6e9ab15e74abfa564729890e28a6
MrMoudDev <santiago.velasquez08120@gmail.com>
2025-06-11 20:17:10 -0500

    Clonacion del repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './component/pages/public/home/home';
 import { Login } from './component/pages/public/login/login';
 import { Registrer } from './component/pages/public/registrer/registrer';
+import { DeveloperProfile } from './pages/private/developer-profile/developer-profile';
 
 export const routes: Routes = [
     {path: "home", component: Home },
     {path: "login", component: Login },
     {path: "registrer", component: Registrer },
+    {path: "dashboard/developerProfile", component: DeveloperProfile },
     {path: "**", redirectTo: "home", pathMatch: "full" },
     {path: "", redirectTo: "home", pathMatch: "full" }
 ];
---
+++ src/app/pages/private/developer-profile/developer-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-developer-profile',
+  imports: [],
+  templateUrl: './developer-profile.html',
+  styleUrl: './developer-profile.css'
+})
+export class DeveloperProfile {
+
+}
