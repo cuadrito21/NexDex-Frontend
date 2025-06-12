26bf9e7b9c51e9d495fc774d21a6cbe308b63d28
Joseph Alejandro Cardenas Palacio <joseph.cardenas2005@gmail.com>
2025-06-11 19:28:21 -0500

    Subiendo el header/main/footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Home } from './component/pages/public/home/home';
 import { Login } from './component/pages/public/login/login';
 import { Registrer } from './component/pages/public/registrer/registrer';
+import { Offers } from './pages/private/offers/offers';
 
 export const routes: Routes = [
     {path: "home", component: Home },
     {path: "login", component: Login },
     {path: "registrer", component: Registrer },
+    {path: "dashboard/offers", component: Offers },
     {path: "**", redirectTo: "home", pathMatch: "full" },
     {path: "", redirectTo: "home", pathMatch: "full" }
 ];
