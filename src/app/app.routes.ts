7798a5b57c31b0bd56140b7553bd769733982598
Cristhiangar <alejandrocrsthian823@gmail.com>
2025-06-12 20:35:15 -0500

    Cambiando el nombre de la ruta de companyProfile a User y agregando el registro de user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,16 @@ import { Home } from './component/pages/public/home/home';
 import { Login } from './component/pages/public/login/login';
 import { Registrer } from './component/pages/public/registrer/registrer';
 import { Offers } from './pages/private/offers/offers';
-import { CompanyProfile } from './pages/private/company-profile/company-profile';
+import { User } from './pages/private/user/user';
+import { UserNewForm } from './pages/private/new-form/new-form';
+
 
 export const routes: Routes = [
     {path: "home", component: Home },
     {path: "login", component: Login },
     {path: "registrer", component: Registrer },
-    {path: "dashboard/companyProfile", component: CompanyProfile},
+    {path: "dashboard/user", component: User},
+    {path: "dashboard/user/new-form", component: UserNewForm},
     {path: "dashboard/offers", component: Offers },
     {path: "**", redirectTo: "home", pathMatch: "full" },
     {path: "", redirectTo: "home", pathMatch: "full" }
