143fc7a2164c1f59c0772f74a102c52e60ea2dea
Cristhiangar <alejandrocrsthian823@gmail.com>
2025-06-13 23:40:04 -0500

    Agregando el home de la pagina con sus respectivas rutas y separando su css

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { Footer } from "./component/content/layout/footer/footer";
-import { Header } from './component/content/layout/header/header';
-import { Main } from "./component/content/layout/main/main";
+import { Header } from './component/content/layout/header/header'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer, Header, Main],
-=======
-import { Header } from "./component/content/layout/header/header";
-import { Footer } from './component/content/layout/footer/footer';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
->>>>>>> 7798a5b57c31b0bd56140b7553bd769733982598
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/pages/private/developer-profile/developer-profile.ts
+++ src/app/pages/private/developer-profile/developer-profile.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
-
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-developer-profile',
-  imports: [RouterLink],
+  imports: [ReactiveFormsModule],
   templateUrl: './developer-profile.html',
   styleUrl: './developer-profile.css'
 })
 export class DeveloperProfile {
+  formData!: FormGroup;
+
+  constructor() {
+    this.formData = new FormGroup({
+      name: new FormControl(),
+      email: new FormControl(),
+      passwort: new FormControl(),
+      role: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    console.log(this.formData.value)
+  }
 
 }
